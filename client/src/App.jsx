5b0fced0574ad72aaaf7ef27df28fb7eba8d61ef
surajshende247 <surajshende247@gmail.com>
2023-09-21 22:41:41 +0530

    feat: dynamic og

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,75 @@
-import "./App.css"
+import { useEffect, useState } from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useSearchParams,
+} from "react-router-dom";
+import "./App.css";
 
-function App() {
+const STORIES = [
+  {
+    id: "1",
+    title: "The First Story",
+  },
+  {
+    id: "2",
+    title: "The Second Story",
+  },
+  {
+    id: "3",
+    title: "The Third Story",
+  },
+];
+
+const StoryPage = () => {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const [story, setStory] = useState({});
+
+  useEffect(() => {
+    if (id) {
+      const story = STORIES.find((story) => story.id === id);
+      setStory(story);
+    }
+  }, [id]);
+
   return (
     <>
       <h1 className="text-center">Open Graph</h1>
+      <div className="container text-center">
+        <h2>{story.title}</h2>
+        <p>{story.description}</p>
+      </div>
     </>
-  )
+  );
+};
+
+const HomePage = () => {
+  return (
+    <>
+      <h1 className="text-center">Open Graph</h1>
+      <div>
+        <a href="/story?id=1">Story 1</a> <br />
+        <a href="/story?id=2">Story 2</a> <br />
+        <a href="/story?id=3">Story 3</a>
+      </div>
+    </>
+  );
+};
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/story",
+    element: <StoryPage />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={routes} />;
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import path from 'path';
 
 const dirname = path.resolve();
@@ -6,14 +7,57 @@ const dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const META_FOR_STORIES = [
+  {
+    id: 1,
+    title: "Story 1",
+    description: "This is the first story",
+    thumbnail: "https://picsum.photos/200/300"
+  },
+  {
+    id: 2,
+    title: "Story 2",
+    description: "This is the second story",
+    thumbnail: "https://picsum.photos/200/300"
+  },
+  {
+    id: 3,
+    title: "Story 3",
+    description: "This is the third story",
+    thumbnail: "https://picsum.photos/200/300"
+  }
+]
+
 app.get("/health", (req, res) => {
   res.send("All good!");
 });
 
-app.use(express.static(path.join(dirname, '..', 'client', 'dist')));
+const indexPath = path.join(dirname, '..', 'client', 'dist', 'index.html');
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(dirname, '..', 'client', 'dist', 'index.html'));
+app.use(express.static(path.resolve(dirname, '..', 'client', 'dist')));
+
+app.get('/*', (req, res, next) => {
+  fs.readFile(indexPath, 'utf8', (err, htmlData) => {
+    if (err) {
+      console.error('Error during file reading', err);
+      return res.status(404).end()
+    }
+
+    const storyId = req.query.id;
+    const story = META_FOR_STORIES.find(story => story.id === Number(storyId));
+    if (!story) {
+      return res.status(404).end();
+    }
+
+    htmlData = htmlData
+      .replace("<title>Stories</title>", `<title>${story.title}</title>`)
+      .replace('__META_OG_TITLE__', story.title)
+      .replace('__META_OG_DESCRIPTION__', story.description)
+      .replace('__META_DESCRIPTION__', story.description)
+      .replace('__META_OG_IMAGE__', story.thumbnail)
+
+    return res.send(htmlData);
+  });
 });
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
